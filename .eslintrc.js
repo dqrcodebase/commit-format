b3f761916912c7c4171417f47c12016b48e4027a
dengqr <852601818@qq.com>
2023-02-08 17:58:50 +0800

    fix: 格式化配置

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,12 +3,17 @@ module.exports = {
   // 所有这些配置文件的规则都会被层叠应用。若有重复的属性配置，
   // 则离文件更近的配置文件具有更高的优先级。
   root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
   // 配置特定于项目中使用的 JavaScript 的语言选项
   env: {
     browser: true,
     node: true,
     es6: true
   },
+  
   extends: ["plugin:vue/recommended", "eslint:recommended", "prettier"],
   // eslint-plugin-prettier 将 Prettier 的规则设置到 ESLint 的规则中。
   // eslint-config-prettier 关闭 ESLint 中与 Prettier 中会发生冲突的规则，优先级：Prettier 配置规则 > ESLint 配置规则。
